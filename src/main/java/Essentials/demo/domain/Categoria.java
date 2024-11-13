0a6e8a1771c077d6480c6209fe1f3ff057745f61
jonathancortesfide <jcortes20382@ufide.ac.cr>
2024-11-12 23:18:15 -0600

    Category added

---
+++ src/main/java/Essentials/demo/domain/Categoria.java
@@ -0,0 +1,33 @@
+package Essentials.demo.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="categories")
+public class Categoria implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Id")
+    private Long id;
+    private String category;
+    private String description;
+    
+    @OneToMany
+    @JoinColumn(name="Id")
+    List<Product> productos;
+
+    public Categoria() {
+    }
+
+    public Categoria(String category, String description) {
+        this.category = category;
+        this.description = description;
+    }
+}
